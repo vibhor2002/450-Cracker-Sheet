52a895f3fd07f3c4c0dfde31724ce031b4d9b707
Vibhor Grover <grovervibhor21@gmail.com>
2022-03-28 20:29:50 +0530

    Array

---
+++ Reverse-the-array-recursive.cpp
@@ -0,0 +1,33 @@
+// Time Complexity o(n)
+
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int start, int end)
+{
+    if(start>=end)
+    return;
+
+    int temp =arr[start];
+    arr[start] =arr[end];
+    arr[end]= temp;
+
+    reverseArray(arr,start+1,end-1);
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, n);
+    reverseArray(arr, 0, n - 1);
+    printArray(arr, n);
+    return 0;
+}
